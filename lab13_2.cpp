37967f373a87b56a70a5ec7b62cec56a475412a2
Ixac <ixacarrrows@gmail.com>
2021-12-24 13:50:20 +0700

    lab13 2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y)
+{
+int  z;
+    if(x%y!=0)
+    {
+        z = gcd(y, x%y);
+       
+    }else 
+    {
+        return y;
+    }
+    return z;
 }
